98910cb06141a1feb5dfa429a28d4c41ddd06f35
ignaciocaamanio <ignacio167@gmail.com>
2017-08-14 02:10:23 -0300

    Version 0.1

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate regex;
+extern crate glob;
+use regex::Regex;
+use std::fs;
+use glob::glob;
+
+fn main() {
+
+    fn foo() -> std::io::Result<()> {
+
+        let re = Regex::new(r".*([sS][0-9][0-9][eE][0-9][0-9]).*").unwrap();
+
+        for entry in fs::read_dir(".")? {
+            let dir = entry?;
+            if re.is_match(&dir.file_name().into_string().unwrap()) {
+                println!("{:?}", dir.path());
+            }
+        }
+
+        Ok(())
+    }
+
+    foo();
+}
+
