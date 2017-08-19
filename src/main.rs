2c171e0f711b4e87c2b92c412ce1cfb7f651e37a
ignaciocaamanio <ignacio167@gmail.com>
2017-08-19 10:06:52 -0300

    versión para avi solamente

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 extern crate regex;
 extern crate glob;
+//extern crate globset;
 use regex::Regex;
-use std::fs;
 use glob::glob;
+use glob::glob_with;
+use glob::MatchOptions;
+//use globset::{Glob, GlobSetBuilder};
 
 fn main() {
 
-    fn iter_path(path: Result<Paths, PatternError>) -> () {
-        
-    }
+    let re = Regex::new(r".*([sS][0-9][0-9][eE][0-9][0-9]).*").unwrap();
 
-    fn foo() -> std::io::Result<()> {
+    for video in glob("*.avi").expect("Failed to read glob pattern") {
+        match video {
+            Ok(path) => {
+                println!("path {}", path.to_str().unwrap());
+                if re.is_match(&path.to_str().unwrap()) {
 
-        let re = Regex::new(r".*([sS][0-9][0-9][eE][0-9][0-9]).*").unwrap();
+                    let season_and_episode = re.captures(path.to_str().unwrap())
+                        .unwrap()
+                        .get(1)
+                        .map_or("", |m| m.as_str());
 
-        for entry in glob("*.srt").expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => if re.is_match(&path) {
-                    println!("{:?}", path);
-                },
-                Err(e) => println!("{:?}", e)
-            }
+                    let options = MatchOptions {
+                        case_sensitive: false,
+                        require_literal_separator: false,
+                        require_literal_leading_dot: false,
+                    };
+
+                    let str_glob = format!("*{}*.srt", season_and_episode);
+                    println!("{}", str_glob);
+
+                    for srt in glob_with(&str_glob, &options).expect("No srt file") {
+                        std::fs::rename(srt.unwrap(), path.with_extension("srt")).unwrap();
+                    }
+                }
+            },
+            Err(e) => println!("{:?}", e)
         }
-        Ok(())
     }
-
-    foo();
 }
-
